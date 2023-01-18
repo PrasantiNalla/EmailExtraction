50d072c7dabff13c1f687ebd1a7377029a45a122
Prasanti <nalla.prasanti@gmail.com>
2023-01-18 14:10:04 +0000

    Email Extraction

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
-import fs from "fs";
-
-
-//const fs = require('fs')
-/*const data = fs.readFile("test.txt",(err, inputD) => {
-   if (err) throw err;
-   console.log(inputD.toString());
-})*/
-//console.log(data.toString());
-/*import { readFileSync } from "fs";
-
-const rawContents = readFileSync("test.txt");
-const contents = rawContents.toString();*/
 
 import { readFileSync } from "fs";
-
 const contents = readFileSync("test.txt", { encoding: "utf-8" });
-
 let emailCounter = 0;
+let softwireEmailcounter = 0;
 
-//emailCounter = contents.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-//console.log(emailCounter.length);
-//let softwireEmailCounter = 0;
-/*softwireEmailCounter = contents.match("@softwire.com");
-console.log(softwireEmailCounter.length);*/
-let counter = 0;
+emailCounter = contents.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+console.log("Total No.of Emails in the file  :" + emailCounter.length);
+
+
 for (let i = 0; i < contents.length; i++) {
 
     if (((contents.substring(i, i + 14) === '@softwire.com ') || contents.substring(i, i + 15) === '@softwire.com\r\n')) {
 
-        counter++;
+        softwireEmailcounter++;
     }
 }
-console.log(counter);
+console.log("Total No.of Softwire Emails in the file  :" + softwireEmailcounter);
